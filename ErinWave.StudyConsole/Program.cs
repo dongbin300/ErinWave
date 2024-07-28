4e6fc4aa4186ebd7987e9a37888b3a139bcbdb82
Roseproject1 <maxguy931028@naver.com>
2024-07-29 01:32:43 +0900

    Study

---
+++ ErinWave.StudyConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ErinWave.StudyConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] bools = { true, false, true, true, true, true, false };
+
+
+            //int count = 0;
+            //for(int i = 0; i < bools.Length; i++)
+            //{
+            //    if (bools[i] == true)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            Console.WriteLine(bools.Count(x => x));
+        }
+    }
+}
